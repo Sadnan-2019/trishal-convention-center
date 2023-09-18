aa9f82b7b978e1ddfa3072ad26089abb36358e41
Sadnan-2019 <sadnan.wow@gmail.com>
2023-09-18 17:25:47 +0600

    landing

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -1,21 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick-theme.css";
 import "slick-carousel/slick/slick.css";
-import "./Landing.css"
- 
+import "./Landing.css";
+import { motion, AnimatePresence } from "framer-motion";
+// import { AnimatePresence } from "framer-motion";
+
 const Landing = () => {
+  const sentences = ["Sentence 1", "Sentence 2", "Sentence 3"];
+  const backgroundImages = [
+    "../../assets/landingone.jpg",
+    "../../assets/landingone.jpg",
+    "../../assets/landingone.jpg",
+  ];
+  console.log(backgroundImages)
+  
+  const transition = { duration: 0.5, ease: "easeInOut" };
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) =>
+        prevIndex === sentences.length - 1 ? 0 : prevIndex + 1
+      );
+    }, 3000);
+
+    return () => clearInterval(interval);
+  }, [sentences.length]);
+
+
 
   var settings = {
-    
     dots: true,
     infinite: true,
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
     initialSlide: 0,
-  
-    autoplay:"true",
+
+    autoplay: "true",
     autoplaySpeed: 5000,
     responsive: [
       {
@@ -24,131 +48,118 @@ const Landing = () => {
           slidesToShow: 1,
           slidesToScroll: 1,
           infinite: true,
-          dots: true
-        }
+          dots: true,
+        },
       },
       {
         breakpoint: 600,
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1,
-          initialSlide: 1
-        }
+          initialSlide: 1,
+        },
       },
       {
         breakpoint: 480,
         settings: {
           slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-    ]
+          slidesToScroll: 1,
+        },
+      },
+    ],
   };
 
   return (
     <div>
-     <div className="  px-7 ">
-
-
-     <Slider {...settings}>
-     
-    
-   
-    
-   <div className="">
-   <div
-         className="hero min-h-screen"
-         style={{
-           backgroundImage: `url("https://www.kcc.rw/uploads/9/8/2/4/98249186/img-9399_orig.jpg")`,
-         }}
-       >
-         <div className="hero-overlay bg-opacity-60"></div>
-         <div className="hero-content text-center text-neutral-content">
-           <div className="max-w-md">
-             <h1 className="mb-5 text-5xl font-bold">Hello paranormal</h1>
-             <p className="mb-5">
-               Provident cupiditate voluptatem et in. Quaerat fugiat ut
-               assumenda excepturi exercitationem quasi. In deleniti eaque
-               aut repudiandae et a id nisi.
-             </p>
-             <button className="btn btn-primary">Get Started</button>
-           </div>
-         </div>
-       </div>
-   </div>
-   
-     
-      <div className="">
-      <div
-         className="hero min-h-screen"
-         style={{
-           backgroundImage: `url("https://images.adsttc.com/media/images/5192/55d3/b3fc/4b8d/f000/005c/large_jpg/MBS_Image_by_BIG_03.jpg?1368544714")`,
-         }}
-       >
-         <div className="hero-overlay bg-opacity-60"></div>
-         <div className="hero-content text-center text-neutral-content">
-           <div className="max-w-md">
-             <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-             <p className="mb-5">
-               Provident cupiditate voluptatem et in. Quaerat fugiat ut
-               assumenda excepturi exercitationem quasi. In deleniti eaque
-               aut repudiandae et a id nisi.
-             </p>
-             <button className="btn btn-primary">Get Started</button>
-           </div>
-         </div>
-       </div>
-     
+      <div className="  px-7 ">
+        <Slider {...settings}>
+          <div className="">
+          <div className="container">
+      <AnimatePresence mode='wait'>
+        <motion.div
+          key={currentIndex}
+          initial={{ opacity: 0, y: "100%" }}
+          animate={{ opacity: 1, y: "0%" }}
+          exit={{ opacity: 0, y: "-100%" }}
+          transition={transition}
+          className="text-container"
+        >
+          <h2>{sentences[currentIndex]}</h2>
+        </motion.div>
+      </AnimatePresence>
+      <div className="background">
+        <img src={backgroundImages[currentIndex]} alt="Background" />
       </div>
-     
-       <div className="">
-       <div
-         className="hero min-h-screen"
-         style={{
-           backgroundImage: `url("https://stcharlesconventioncenter.com/assets/images/galleries/full/Szqtm.jpeg")`,
-         }}
-       >
-         <div className="hero-overlay bg-opacity-60"></div>
-         <div className="hero-content text-center text-neutral-content">
-           <div className="max-w-md">
-             <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-             <p className="mb-5">
-               Provident cupiditate voluptatem et in. Quaerat fugiat ut
-               assumenda excepturi exercitationem quasi. In deleniti eaque
-               aut repudiandae et a id nisi.
-             </p>
-             <button className="btn btn-primary">Get Started</button>
-           </div>
-         </div>
-       </div>
-       </div>
-     
-     
-      <div className="">
-      <div
-         className="hero min-h-screen"
-         style={{
-           backgroundImage: `url("https://placeimg.com/1000/800/arch")`,
-         }}
-       >
-         <div className="hero-overlay bg-opacity-60"></div>
-         <div className="hero-content text-center text-neutral-content">
-           <div className="max-w-md">
-             <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-             <p className="mb-5">
-               Provident cupiditate voluptatem et in. Quaerat fugiat ut
-               assumenda excepturi exercitationem quasi. In deleniti eaque
-               aut repudiandae et a id nisi.
-             </p>
-             <button className="btn btn-primary">Get Started</button>
-           </div>
-         </div>
-       </div>
+    </div>
+          </div>
+
+          {/* <div className="">
+            <div
+              className="hero min-h-screen"
+              style={{
+                backgroundImage: `url("https://images.adsttc.com/media/images/5192/55d3/b3fc/4b8d/f000/005c/large_jpg/MBS_Image_by_BIG_03.jpg?1368544714")`,
+              }}
+            >
+              <div className="hero-overlay bg-opacity-60"></div>
+              <div className="hero-content text-center text-neutral-content">
+                <div className="max-w-md">
+                  <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+                  <p className="mb-5">
+                    Provident cupiditate voluptatem et in. Quaerat fugiat ut
+                    assumenda excepturi exercitationem quasi. In deleniti eaque
+                    aut repudiandae et a id nisi.
+                  </p>
+                  <button className="btn btn-primary">Get Started</button>
+                </div>
+              </div>
+            </div>
+          </div> */}
+
+          {/* <div className="">
+            <div
+              className="hero min-h-screen"
+              style={{
+                backgroundImage: `url("https://stcharlesconventioncenter.com/assets/images/galleries/full/Szqtm.jpeg")`,
+              }}
+            >
+              <div className="hero-overlay bg-opacity-60"></div>
+              <div className="hero-content text-center text-neutral-content">
+                <div className="max-w-md">
+                  <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+                  <p className="mb-5">
+                    Provident cupiditate voluptatem et in. Quaerat fugiat ut
+                    assumenda excepturi exercitationem quasi. In deleniti eaque
+                    aut repudiandae et a id nisi.
+                  </p>
+                  <button className="btn btn-primary">Get Started</button>
+                </div>
+              </div>
+            </div>
+          </div> */}
+
+          {/* <div className="">
+            <div
+              className="hero min-h-screen"
+              style={{
+                backgroundImage: `url("https://placeimg.com/1000/800/arch")`,
+              }}
+            >
+              <div className="hero-overlay bg-opacity-60"></div>
+              <div className="hero-content text-center text-neutral-content">
+                <div className="max-w-md">
+                  <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+                  <p className="mb-5">
+                    Provident cupiditate voluptatem et in. Quaerat fugiat ut
+                    assumenda excepturi exercitationem quasi. In deleniti eaque
+                    aut repudiandae et a id nisi.
+                  </p>
+                  <button className="btn btn-primary">Get Started</button>
+                </div>
+              </div>
+            </div>
+          </div> */}
+        </Slider>
       </div>
-      
-      
-   </Slider>
-     </div>
     </div>
   );
 };
