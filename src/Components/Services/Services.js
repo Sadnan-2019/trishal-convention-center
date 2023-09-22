78ef16e4348af7ebeeb8e84d6e790e89881634f6
Sadnan-2019 <54290526+Sadnan-2019@users.noreply.github.com>
2023-09-22 23:07:09 +0600

    nav bar

---
+++ src/Components/Services/Services.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Services = () => {
+  return (
+    <div>
+      <div className=" ">
+        <h2
+          className="text-transparent   bg-clip-text bg-gradient-to-r from-[#034ca7] to-[#2622B7]      text-3xl text-center  py-8 font-bold poppins-b sm:text-4xl "
+          // style={{ color: "#536E9B" }}
+          data-aos="fade-up"
+          data-aos-duration="1000"
+        >
+          Our Services
+        </h2>
+      </div>
+
+      <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 px-0   lg:px-12 gap-10    py-[25px] items-center justify-items-center "></div>
+    </div>
+  );
+};
+
+export default Services;
