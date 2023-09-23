27d1b4004decf62871d29ff7489e2ccbbda280fb
Sadnan-2019 <sadnan.wow@gmail.com>
2023-09-23 17:49:17 +0600

    Service

--- src/Components/Services/Services.js
+++ src/Components/Services/Services.js
@@ -1,5 +1,10 @@
 import React from "react";
-import 'imagehover.css/css/imagehover.css'; 
+import tgr from "../../assets/tgr.jpg"
+import tgrone from "../../assets/tgrone.png"
+import tgrtwo from "../../assets/tgr2.jpg"
+import tgrthree from "../../assets/tgr3.png"
+import "./Services.css";
+
 const Services = () => {
   return (
     <div>
@@ -15,28 +20,119 @@ const Services = () => {
       </div>
 
       <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 px-0   lg:px-12 gap-10    py-[25px] items-center justify-items-center ">
-      <figure class="imghvr-stack-up">
-  <img src="#" alt="example-image" />
-  <figcaption>
-    <h3 class="ih-fade-down ih-delay-sm ">Oscar Wilde</h3>
-    <p class="ih-zoom-in ih-delay-md">
-      <i>"I have the simplest tastes, I am always satisfied with the best."</i>
-    </p>
-    <a class="ih-fade-up ih-delay-lg button" href="#">Read More</a>
-  </figcaption>
-  <a href="#"></a>
-  <figure class="imghvr-fade">
-  <img src="#"/>
-  <figcaption>
-    // Hover Content
-  </figcaption>
-  <a href="http://www.imagehover.io"></a>
-</figure>
-</figure>
+        <div class="         text-white     max-w-sm shadow-2xl   bg-gradient-to-r from-[#034ca7] to-[#2622B7]    ">
+          <div className="       ">
+              
 
-
-
-
+            <div class="section over-hide bg-blue-500">
+              <div class="container  ">
+                <div class="row full-height justify-content-center">
+                  <div class="col-12 text-center align-self-center py-5">
+                    <div class="section text-center py-5 py-md-0">
+                      <input
+                        class="pricing"
+                        type="checkbox"
+                        id="pricing"
+                        name="pricing"
+                      />
+                      <label for="pricing">
+                        <span class="block-diff">
+                          kayaking<span class="float-right">camping</span>
+                        </span>
+                      </label>
+                      <div class="card-3d-wrap mx-auto">
+                        <div class="card-3d-wrapper">
+                          <div class="card-front">
+                            <div class="pricing-wrap">
+                              {/* <h4 class="mb-5">Kayaking</h4> */}
+                              {/* <h2 class="mb-2">
+                                <sup>$</sup>39 / 4<sup>hrs</sup>
+                              </h2> */}
+                              {/* <p class="mb-4">per person</p> */}
+                              <p class="mb-1">
+                                <i class="uil uil-location-pin-alt size-22"></i>
+                              </p>
+                              {/* <p class="mb-4">Drina, Serbia</p>
+                              <a href="#0" class="link">
+                                Choose Date
+                              </a> */}
+                              <div class="img-wrap img-2">
+                                <img
+                                  src={tgr}
+                                  alt=""
+                                />
+                              </div>
+                              {/* <div class="img-wrap img-1">
+                                <img
+                                  src="https://assets.codepen.io/1462889/kayak.png"
+                                  alt=""
+                                />
+                              </div> */}
+                              {/* <div class="img-wrap img-3">
+                                <img
+                                  src="https://assets.codepen.io/1462889/water.png"
+                                  alt=""
+                                />
+                              </div> */}
+                              {/* <div class="img-wrap img-6">
+                                <img
+                                  src="https://assets.codepen.io/1462889/Stone.png"
+                                  alt=""
+                                />
+                              </div> */}
+                            </div>
+                          </div>
+                          <div class="card-back">
+                            <div class="pricing-wrap">
+                              {/* <h4 class="mb-5">Camping</h4>
+                              <h2 class="mb-2">
+                                <sup>$</sup>29 / 8<sup>hrs</sup>
+                              </h2>
+                              <p class="mb-4">per person</p>
+                              <p class="mb-1">
+                                <i class="uil uil-location-pin-alt size-22"></i>
+                              </p>
+                              <p class="mb-4">Tara, Serbia</p> */}
+                             
+                              <div class="   ">
+                                <img
+                                  src={tgrtwo}
+                                  alt=""
+                                />
+                              </div>
+                              <div class=" img-wrap  img-4  ">
+                                <img
+                                  src={tgrone}
+                                  alt=""
+                                  className="rounded-full w-32 h-6"
+                                />
+                              </div>
+                              <div class="img-wrap img-5">
+                                <img
+                                  src={tgrthree}
+                                  alt=""
+                                />
+                              </div>
+                              {/* <div class="img-wrap img-7">
+                                <img
+                                  src="https://assets.codepen.io/1462889/IvyRock.png"
+                                  alt=""
+                                />
+                              </div> */}
+                               <a href="#0" className=" link">
+                                Details
+                              </a>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
