546d4270403e66329ac4a1ecc97114f8542d3636
Sadnan-2019 <sadnan.wow@gmail.com>
2023-11-08 16:58:15 +0600

    food

---
+++ src/Components/Rose/RoseDetails.js
@@ -0,0 +1,173 @@
+import React from 'react';
+import waterone from "../../assets/rose1.jpg";
+import watertwo from "../../assets/rose7.jpg";
+import waterthree from "../../assets/rose11.jpg";
+import waterfour from "../../assets/rose12.jpg";
+import waterfive from "../../assets/rose2.jpg";
+import watersix from "../../assets/rose3.jpg";
+import waterseven from "../../assets/rose4.jpg";
+const RoseDetails = () => {
+    return (
+        <div>
+            <div className="bg-gradient-to-r from-[#9e9eab] via-[#160a2c] to-[#38235D] ">
+      <div class="   ">
+        <div class="grid grid-cols-1 gap-8 mt-8 md:grid-cols-2 lg:grid-cols-2   items-center justify-items-center py-10">
+          <div class="text-white new-details hover:bg-gray-900 hover:text-white transition duration-300 max-w-lg rounded overflow-hidden shadow-lg">
+            <div class="py-4 px-8 max-w-lg ">
+              {/* <img src="https://tailwindcss.com/img/jonathan.jpg" class="rounded-full h-12 w-12 mb-4"/> */}
+              <a href="#">
+                <h4 class="text-lg mb-3 font-semibold">
+                  How to be effective at working remotely?
+                </h4>
+              </a>
+              <p class="text-white mb-2 text-sm  ">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s
+              </p>
+              <img src={waterone}   class=" w-full h-96 rounded-lg" alt="" />
+              <hr class="mt-4" />
+              <span class="text-xs">ARTICLE</span>
+              &nbsp;<span class="text-xs text-gray-500">PROCESS</span>
+            </div>
+          </div>
+          <div class="text-white new-details hover:bg-gray-900 hover:text-white transition duration-300 max-w-lg rounded overflow-hidden shadow-lg">
+            <div class="py-4 px-8">
+              {/* <img src="https://tailwindcss.com/img/jonathan.jpg" class="rounded-full h-12 w-12 mb-4"/> */}
+              <a href="#">
+                <h4 class="text-lg mb-3 font-semibold">
+                  How to be effective at working remotely?
+                </h4>
+              </a>
+              <p class="text-white mb-2 text-sm ">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s
+              </p>
+              <img
+                src={watertwo} class=" w-full h-96 rounded-lg" alt=""
+                 
+              />
+              <hr class="mt-4" />
+              <span class="text-xs">ARTICLE</span>
+              &nbsp;<span class="text-xs text-gray-500">PROCESS</span>
+            </div>
+          </div>
+          <div class="text-white new-details hover:bg-gray-900 hover:text-white transition duration-300 max-w-lg rounded overflow-hidden shadow-lg">
+            <div class="py-4 px-8 ">
+              {/* <img src="https://tailwindcss.com/img/jonathan.jpg" class="rounded-full h-12 w-12 mb-4"/> */}
+              <a href="#">
+                <h4 class="text-lg mb-3 font-semibold">
+                  How to be effective at working remotely?
+                </h4>
+              </a>
+              <p class="text-white mb-2 text-sm  ">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s
+              </p>
+              <img
+                src={waterthree}
+                class=" w-full h-96 rounded-lg" alt=""
+              />
+              <hr class="mt-4" />
+              <span class="text-xs">ARTICLE</span>
+              &nbsp;<span class="text-xs text-gray-500">PROCESS</span>
+            </div>
+          </div>
+          <div class="text-white new-details hover:bg-gray-900 hover:text-white transition duration-300 max-w-lg rounded overflow-hidden shadow-lg">
+            <div class="py-4 px-8">
+              {/* <img src="https://tailwindcss.com/img/jonathan.jpg" class="rounded-full h-12 w-12 mb-4"/> */}
+              <a href="#">
+                <h4 class="text-lg mb-3 font-semibold">
+                  How to be effective at working remotely?
+                </h4>
+              </a>
+              <p class="text-white mb-2 text-sm  ">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s
+              </p>
+              <img
+                src={waterfour}
+                class=" w-full h-96 rounded-lg" alt=''
+              />
+              <hr class="mt-4" />
+              <span class="text-xs">ARTICLE</span>
+              &nbsp;<span class="text-xs text-gray-500">PROCESS</span>
+            </div>
+          </div>
+          <div class="text-white new-details hover:bg-gray-900 hover:text-white transition duration-300 max-w-lg rounded overflow-hidden shadow-lg">
+            <div class="py-4 px-8">
+              {/* <img src="https://tailwindcss.com/img/jonathan.jpg" class="rounded-full h-12 w-12 mb-4"/> */}
+              <a href="#">
+                <h4 class="text-lg mb-3 font-semibold">
+                  How to be effective at working remotely?
+                </h4>
+              </a>
+              <p class="text-white mb-2 text-sm  ">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s
+              </p>
+              <img
+                src={waterfive}
+                class=" w-full h-96 rounded-lg" alt=""
+              />
+              <hr class="mt-4" />
+              <span class="text-xs">ARTICLE</span>
+              &nbsp;<span class="text-xs text-gray-500">PROCESS</span>
+            </div>
+          </div>
+          <div class="text-white new-details hover:bg-gray-900 hover:text-white transition duration-300 max-w-lg rounded overflow-hidden shadow-lg">
+            <div class="py-4 px-8">
+              {/* <img src="https://tailwindcss.com/img/jonathan.jpg" class="rounded-full h-12 w-12 mb-4"/> */}
+              <a href="#">
+                <h4 class="text-lg mb-3 font-semibold">
+                  How to be effective at working remotely?
+                </h4>
+              </a>
+              <p class="text-white mb-2 text-sm  ">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s
+              </p>
+              <img
+                src={watersix}
+                class=" w-full h-96 rounded-lg" alt=""
+              />
+              <hr class="mt-4" />
+              <span class="text-xs">ARTICLE</span>
+              &nbsp;<span class="text-xs text-gray-500">PROCESS</span>
+            </div>
+          </div>
+          <div class="text-white new-details hover:bg-gray-900 hover:text-white transition duration-300 max-w-lg rounded overflow-hidden shadow-lg">
+            <div class="py-4 px-8">
+              {/* <img src="https://tailwindcss.com/img/jonathan.jpg" class="rounded-full h-12 w-12 mb-4"/> */}
+              <a href="#">
+                <h4 class="text-lg mb-3 font-semibold">
+                  How to be effective at working remotely?
+                </h4>
+              </a>
+              <p class="text-white mb-2 text-sm  ">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s
+              </p>
+              <img
+                src={waterseven}
+                class=" w-full h-96 rounded-lg" alt=""
+              />
+              <hr class="mt-4" />
+              <span class="text-xs">ARTICLE</span>
+              &nbsp;<span class="text-xs text-gray-500">PROCESS</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+        </div>
+    );
+};
+
+export default RoseDetails;
