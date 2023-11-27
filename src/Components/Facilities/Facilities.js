05993d322065a03ff868a0f2927015b32ced6f93
Sadnan-2019 <sadnan.wow@gmail.com>
2023-11-27 12:35:40 +0600

    facilities

--- src/Components/Facilities/Facilities.js
+++ src/Components/Facilities/Facilities.js
@@ -6,6 +6,7 @@ import calender from "../../assets/facebook.png";
 import ux from "../../assets/twitter.png";
 import congrat from "../../assets/instagram.png";
 import wifi from "../../assets/wifi.avif";
+import cctv from "../../assets/cctv.avif";
 
 import "./Facilities.css";
 
@@ -24,21 +25,19 @@ const Facilities = () => {
           </h2>
         </div>
 
-        <div class="container my-12 mx-auto md:px-6 ">
+        <div class="container my-12  md:px-6 ">
           <section class=" relative  text-center">
-            <div class=" flex flex-wrap justify-center">
-              <div class="  shrink-0 grow-0 basis-auto px-3 md:w-10/12">
+            <div class="  ">
+            
                 <div
-                  class=" mb-6 overflow-hidden rounded-lg bg-cover bg-no-repeat shadow-lg dark:shadow-black/20"
+                  class="mb-6 flex mx-auto flex-wrap  justify-center items-center overflow-hidden rounded-lg  px-3 lg:w-2/3   "
                   data-te-ripple-init
                   data-te-ripple-color="light"
                 >
-                  <img src={wifi} class=" " />
-                </div>
-              </div>
+                  <img src={wifi} class="lg:h-1/4 lg:w-2/3 rounded-lg " alt=""/>
 
-              <div class="w-full shrink-0 grow-0 basis-auto px-3 md:w-8/12 xl:w-6/12 text-indigo-100">
-                <h5 class="mb-3 text-2xl font-bold">High-Speed Free Wi-Fi</h5>
+                  <div class="w-full shrink-0 grow-0 basis-auto px-3   text-indigo-100">
+                <h5 class="py-5 text-2xl font-bold">High-Speed Free Wi-Fi</h5>
 
                 <p class="mb-6">
                   We are living in a digital era. So social media is very
@@ -48,6 +47,9 @@ const Facilities = () => {
                   or speed. Just connect yourself to the digital world easily.
                 </p>
               </div>
+                </div>
+            
+              
             </div>
             <div
               data-aos="fade-up"
@@ -179,7 +181,7 @@ const Facilities = () => {
         <div class="container my-12 mx-auto md:px-6 ">
           <section class=" relative  text-center">
             <div class=" flex flex-wrap justify-center">
-              <div class="  shrink-0 grow-0 basis-auto px-3 md:w-10/12">
+              <div class="  shrink-0 grow-0 basis-auto px-3 md:w-2/3">
                 <div
                   class=" mb-6 overflow-hidden rounded-lg bg-cover bg-no-repeat shadow-lg dark:shadow-black/20"
                   data-te-ripple-init
@@ -198,6 +200,157 @@ const Facilities = () => {
                   have large underground facility with 100+ car parking
                   capability. So you don’t need to worry about your guest’s car
                   parking space at all.
+                </p>
+              </div>
+            </div>
+            <div
+              data-aos="fade-up"
+              data-aos-delay="300"
+              data-aos-once="true"
+              class="absolute top-20 -left-6 sm:top-32 sm:left-10 md:top-40 md:left-16 lg:-left-1  floating-4"
+            >
+              <img
+                class="bg-white bg-opacity-80 rounded-lg h-12 sm:h-16 lg:h-12 w-12 lg:-left-[20px]  "
+                src={calender}
+                alt=""
+              />
+            </div>
+
+            <div
+              data-aos="fade-up"
+              data-aos-delay="400"
+              data-aos-once="true"
+              class="absolute top-20 right-10 sm:right-24 sm:top-28 md:top-36 md:right-32 lg:top-32 lg:right-16 floating"
+            >
+              <svg
+                class="h-16 sm:h-24"
+                viewBox="0 0 149 149"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <g filter="url(#filter0_d)">
+                  <rect
+                    x="40"
+                    y="32"
+                    width="69"
+                    height="69"
+                    rx="14"
+                    fill="#F3627C"
+                  />
+                </g>
+                <rect
+                  x="51.35"
+                  y="44.075"
+                  width="47.3"
+                  height="44.85"
+                  rx="8"
+                  fill="white"
+                />
+                <path
+                  d="M74.5 54.425V78.575"
+                  stroke="#F25471"
+                  stroke-width="4"
+                  stroke-linecap="round"
+                />
+                <path
+                  d="M65.875 58.7375L65.875 78.575"
+                  stroke="#F25471"
+                  stroke-width="4"
+                  stroke-linecap="round"
+                />
+                <path
+                  d="M83.125 63.9125V78.575"
+                  stroke="#F25471"
+                  stroke-width="4"
+                  stroke-linecap="round"
+                />
+                <defs>
+                  <filter
+                    id="filter0_d"
+                    x="0"
+                    y="0"
+                    width="149"
+                    height="149"
+                    filterUnits="userSpaceOnUse"
+                    color-interpolation-filters="sRGB"
+                  >
+                    <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                    <feColorMatrix
+                      in="SourceAlpha"
+                      type="matrix"
+                      values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+                    />
+                    <feOffset dy="8" />
+                    <feGaussianBlur stdDeviation="20" />
+                    <feColorMatrix
+                      type="matrix"
+                      values="0 0 0 0 0.825 0 0 0 0 0.300438 0 0 0 0 0.396718 0 0 0 0.26 0"
+                    />
+                    <feBlend
+                      mode="normal"
+                      in2="BackgroundImageFix"
+                      result="effect1_dropShadow"
+                    />
+                    <feBlend
+                      mode="normal"
+                      in="SourceGraphic"
+                      in2="effect1_dropShadow"
+                      result="shape"
+                    />
+                  </filter>
+                </defs>
+              </svg>
+            </div>
+
+            <div
+              data-aos="fade-up"
+              data-aos-delay="500"
+              data-aos-once="true"
+              class="absolute bottom-14 -left-4 sm:left-2 sm:bottom-20   lg:-lef-1 floating"
+            >
+              <img
+                class="bg-white bg-opacity-80 rounded-lg  lg:h-12 w-12 sm:h-28"
+                src={congrat}
+                alt=""
+              />
+            </div>
+
+            <div
+              data-aos="fade-up"
+              data-aos-delay="600"
+              data-aos-once="true"
+              class="absolute bottom-20 md:bottom-48 lg:bottom-52 -right-6 lg:right-8 floating-4"
+            >
+              <img
+                class="bg-white bg-opacity-80 rounded-lg h-12 sm:h-16"
+                src={ux}
+                alt=""
+              />
+            </div>
+          </section>
+        </div>
+        <div class="container my-12 mx-auto md:px-6 ">
+          <section class=" relative  text-center">
+            <div class=" flex flex-wrap justify-center">
+              <div class="  shrink-0 grow-0 basis-auto px-3 md:w-2/3">
+                <div
+                  class=" mb-6 overflow-hidden rounded-lg bg-cover bg-no-repeat shadow-lg dark:shadow-black/20"
+                  data-te-ripple-init
+                  data-te-ripple-color="light"
+                >
+                  <img src={cctv} class="h-1/2 " />
+                </div>
+              </div>
+
+              <div class="w-full shrink-0 grow-0 basis-auto px-3 md:w-8/12 xl:w-6/12 text-indigo-100">
+                <h5 class="mb-3 text-2xl font-bold">24/7 CCTV Coverage</h5>
+
+                <p class="mb-6">
+                  We all need security and safety. So having a surveillance
+                  system is very important. As we may not know when something
+                  unexpected happens. Our halls are equipped with 24/7 CCTV
+                  surveillance and every action is being monitored properly. So
+                  if anythings unexpected happens, we can help our clients.
                 </p>
               </div>
             </div>
